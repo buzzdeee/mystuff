25c67ed18fd2f7e2f77c0da9c58163974e047951
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2022-10-21 15:51:35 +0200

    Add sysutils/ruby-facter

---
+++ sysutils/ruby-facter/files/resolvers/dhcp.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../networking'
+
+module Facter
+  module Resolvers
+    module Openbsd
+      class Dhcp < Facter::Resolvers::Networking
+        init_resolver
+        class << self
+          def extract_dhcp(interface_name, raw_data, parsed_interface_data)
+            result = Facter::Core::Execution.execute("dhcpleasectl -l #{interface_name}" , logger: log)
+            unless result.empty?
+              match = result.match(/^#{interface_name} \[Bound\]$/)&.to_s&.strip
+              parsed_interface_data[:dhcp] = interface_name unless match.empty?
+            end
+          end
+        end
+      end
+    end
+  end
+end
