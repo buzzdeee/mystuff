6da5dc67a3c2e287c765226c5789899e45fb0062
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2022-10-25 22:03:04 +0200

    add pupppetserver, not yet starting up

---
+++ sysutils/puppetserver/files/cli-defaults.sh
@@ -0,0 +1,2 @@
+INSTALL_DIR="/usr/local/share/puppetlabs/puppetserver"
+CLASSPATH="${CLASSPATH}:/usr/local/lib/ruby/vendor_ruby/${MODRUBY_REV}/facter.jar:/usr/local/share/puppetlabs/puppetserver/jars/*"
