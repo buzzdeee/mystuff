5951b1d3c458bf19008d5ebc7b7e6fa50e14517a
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2023-01-17 21:41:26 +0100

    move puppetserver to puppetserver/7 branch

--- sysutils/puppetserver/files/cli-defaults.sh
+++
@@ -1,2 +0,0 @@
-INSTALL_DIR="/usr/local/share/puppetlabs/puppetserver"
-CLASSPATH="${CLASSPATH}:/usr/local/lib/ruby/vendor_ruby/${MODRUBY_REV}/facter.jar:/usr/local/share/puppetlabs/puppetserver/jars/*"
